0bf00a6ac01c64837f5b84a5af1c89b0cb8f387c
LuAnderson <lucasand96@gmail.com>
2019-03-17 15:19:16 -0300

    Simple modify in body tag

--- app/app.module.js
+++ app/app.module.js
@@ -2,13 +2,13 @@
 
 var app = angular.module("wooza", []);
 
-app.controller("getjson", ["$scope", "$http", function ($scope, $http) {
+app.controller("mainController", ["$scope", "$http", function ($scope, $http) {
     $http({
         method: 'GET',
         url: 'http://private-59658d-celulardireto2017.apiary-mock.com/plataformas'
     }).then(function (response) {
         console.log("Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
-        $scope.data = response.data;
+        $scope.produtos = response.data;
     }, function (error) {
         console.log("Não foi possível conectar com a API, erro: ", error);
     });
