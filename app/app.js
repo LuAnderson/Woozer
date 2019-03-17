f1510108b63df4506bf353faf42b1a531065343f
LuAnderson <lucasand96@gmail.com>
2019-03-17 20:03:02 -0300

    POC - Router

--- app/app.js
+++ app/app.js
@@ -7,6 +7,23 @@ var app = angular.module('wooza', ['ngRoute']).config(
                 controller: 'cardProdutos',
                 controllerAs: 'vm'
             })
+            .when('/tablet', {
+                templateUrl: './components/planos/planos.component.html',
+                controller: 'tabletProdutos',
+                controllerAs: 'vm'
+            })
+            .when('/computador', {
+                templateUrl: './components/planos/planos.component.html',
+                controller: 'computadorProdutos',
+                controllerAs: 'vm'
+            })
+            .when('/wifi', {
+                templateUrl: './components/planos/planos.component.html',
+                controller: 'wifiProdutos',
+                controllerAs: 'vm'
+            }).otherwise({
+                redirectTo: '/'
+            });
     });
 
 
@@ -16,6 +33,42 @@ app.controller("cardProdutos", ["$scope", "$http", function ($scope, $http) {
         url: 'http://private-59658d-celulardireto2017.apiary-mock.com/plataformas'
     }).then(function (response) {
         console.log("Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
+        $scope.produtos = response.data;
+    }, function (error) {
+        console.log("Não foi possível conectar com a API, erro: ", error);
+    });
+}]);
+
+app.controller("tabletProdutos", ["$scope", "$http", function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/TBT01'
+    }).then(function (response) {
+        console.log("(tabletProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
+        $scope.produtos = response.data;
+    }, function (error) {
+        console.log("Não foi possível conectar com a API, erro: ", error);
+    });
+}]);
+
+app.controller("computadorProdutos", ["$scope", "$http", function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/CPT02'
+    }).then(function (response) {
+        console.log("(computadorProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
+        $scope.produtos = response.data;
+    }, function (error) {
+        console.log("Não foi possível conectar com a API, erro: ", error);
+    });
+}]);
+
+app.controller("wifiProdutos", ["$scope", "$http", function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/WF03'
+    }).then(function (response) {
+        console.log("(wifiProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
         $scope.produtos = response.data;
     }, function (error) {
         console.log("Não foi possível conectar com a API, erro: ", error);
