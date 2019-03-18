0961763ccc4df08d6b2974b9c3357c870d16a403
LuAnderson <lucasand96@gmail.com>
2019-03-18 16:16:39 -0300

    Add Factory in Project Controller Simplify

--- app/app.js
+++ app/app.js
@@ -30,60 +30,160 @@ var app = angular
                     controller: 'formUser',
                     controllerAs: 'vm'
                 })
-                .otherwise({ redirectTo: '/' }); 
+                .otherwise({ redirectTo: '/' });
         });
 
 
-app.controller("cardProdutos", ["$scope", "$http", function ($scope, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/plataformas'
-    }).then(function (response) {
-        console.log("Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
-        $scope.produtos = response.data;
-    }, function (error) {
-        console.log("Não foi possível conectar com a API, erro: ", error);
-    });
-}]);
+app.controller("cardProdutos", function ($scope, Produtos) {
+    $scope.produtos = {};
 
-app.controller("tabletProdutos", ["$scope", "$http", function ($scope, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/TBT01'
-    }).then(function (response) {
-        console.log("(tabletProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
-        $scope.produtos = response.data;
-    }, function (error) {
-        console.log("Não foi possível conectar com a API, erro: ", error);
+    Produtos.getProdutos(function (obj) {
+        $scope.produtos = obj.data;
     });
-}]);
+});
 
-app.controller("computadorProdutos", ["$scope", "$http", function ($scope, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/CPT02'
-    }).then(function (response) {
-        console.log("(computadorProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
-        $scope.produto = response.data;
-    }, function (error) {
-        console.log("Não foi possível conectar com a API, erro: ", error);
-    });
-}]);
+app.controller("tabletProdutos", function ($scope, Tablet) {
+    $scope.produtos = {};
 
-app.controller("wifiProdutos", ["$scope", "$http", function ($scope, $http) {
-    $http({
-        method: 'GET',
-        url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/WF03'
-    }).then(function (response) {
-        console.log("(wifiProdutos) Resposta Obtida:", response.status, "retornando as seguintes informações: ", response.data);
-        $scope.produto = response.data;
-    }, function (error) {
-        console.log("Não foi possível conectar com a API, erro: ", error);
+    Tablet.getTablet(function (obj) {
+        $scope.produtos = obj.data;
     });
-}]);
+});
+
+app.controller("computadorProdutos", function ($scope, Computador) {
+    $scope.produto = {};
+
+    Computador.getComputador(function (obj) {
+        $scope.produto = obj.data;
+    });
+});
+
+app.controller("wifiProdutos", function ($scope, Wifi) {
+    $scope.produto = {};
+
+    Wifi.getWifi(function (obj) {
+        $scope.produto = obj.data;
+    });
+});
 
 app.controller('formUser', function ($scope) {
     $scope.sendForm = function () {
         console.log("$scope.userInfo: ", $scope.userInfo);
     };
 });
+
+app.factory('Produtos', function ($http) {
+    var produtosList;
+    var obj = {};
+
+    obj = {
+        getProdutos: function (callback) {
+            if (produtosList) {
+                callback(produtosList);
+                return false;
+            } else {
+
+                $http({
+                    method: 'GET',
+                    url: 'http://private-59658d-celulardireto2017.apiary-mock.com/plataformas'
+                }).then(function (data) {
+                    obj.saveProdutos(data);
+                    callback(data);
+
+                })
+            }
+        },
+        saveProdutos: function (data) {
+            produtosList = data;
+        }
+    }
+
+    return obj;
+})
+
+app.factory('Tablet', function ($http) {
+    var tabletList;
+    var obj = {};
+
+    obj = {
+        getTablet: function (callback) {
+            if (tabletList) {
+                callback(tabletList);
+                return false;
+            } else {
+
+                $http({
+                    method: 'GET',
+                    url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/TBT01'
+                }).then(function (data) {
+                    obj.saveTablet(data);
+                    callback(data);
+
+                })
+            }
+        },
+        saveTablet: function (data) {
+            tabletList = data;
+        }
+    }
+
+    return obj;
+})
+
+app.factory('Computador', function ($http) {
+    var wifiList;
+    var obj = {};
+
+    obj = {
+        getComputador: function (callback) {
+            if (computadorList) {
+                callback(computadorList);
+                return false;
+            } else {
+
+                $http({
+                    method: 'GET',
+                    url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/CPT02'
+                }).then(function (data) {
+                    obj.saveComputador(data);
+                    callback(data);
+
+                })
+            }
+        },
+        saveComputador: function (data) {
+            computadorList = data;
+        }
+    }
+
+    return obj;
+})
+
+app.factory('Wifi', function ($http) {
+    var wifiList;
+    var obj = {};
+
+    obj = {
+        getWifi: function (callback) {
+            if (wifiList) {
+                callback(wifiList);
+                return false;
+            } else {
+
+                $http({
+                    method: 'GET',
+                    url: 'http://private-59658d-celulardireto2017.apiary-mock.com/planos/CPT02'
+                }).then(function (data) {
+                    obj.saveWifi(data);
+                    callback(data);
+
+                })
+            }
+        },
+        saveWifi: function (data) {
+            wifiList = data;
+        }
+    }
+
+    return obj;
+})
